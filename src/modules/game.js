ebb7378bdc2c43c9590d84339fb47bc0b9afb3e2
mantesso <fmantesso@gmail.com>
2024-02-16 15:25:51 +0100

    game module change

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,16 +1,17 @@
-const Gameboard = require("./gameboard");
+const Player = require("./player");
 const { updatePlayerBoard, updateEnemyBoard } = require("./ui");
 
-let gameboard = new Gameboard();
-gameboard.placeShip(3, [1, 1], "h");
-gameboard.placeShip(4, [3, 3], "v");
-gameboard.receiveAttack([0, 0]);
-gameboard.receiveAttack([6, 6]);
-gameboard.receiveAttack([1, 1]);
+let player = new Player();
+player.gameboard.placeShip(3, [1, 1], "h");
+player.gameboard.placeShip(4, [3, 3], "v");
+player.gameboard.receiveAttack([0, 0]);
+player.gameboard.receiveAttack([6, 6]);
+player.gameboard.receiveAttack([1, 1]);
+console.log(player.randomAttack());
 
-let enemy = new Gameboard();
-enemy.placeShip(2, [4, 1], "h");
-enemy.placeShip(1, [0, 0], "v");
+let enemy = new Player();
+enemy.gameboard.placeShip(2, [4, 1], "h");
+enemy.gameboard.placeShip(1, [0, 0], "v");
 
-updatePlayerBoard(gameboard);
-updateEnemyBoard(enemy);
+updatePlayerBoard(player.gameboard);
+updateEnemyBoard(enemy.gameboard);
