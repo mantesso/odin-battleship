8abbc9ef87e2f2367645cfe5f19ec1cfb0d80621
mantesso <fmantesso@gmail.com>
2024-02-18 15:58:48 +0100

    refactor markBufferZoneH and markBufferZoneV

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -150,79 +150,12 @@ class Gameboard {
   }
 
   markBufferZoneH(coord, i, shipLength) {
-    let neighbors;
-    if (shipLength == 1) {
-      neighbors = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, -1],
-        [0, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-      ];
-    } else if (i == 0) {
-      neighbors = [
-        [-1, -1],
-        [-1, 0],
-        [0, -1],
-        [1, -1],
-        [1, 0],
-      ];
-    } else if (i == shipLength - 1) {
-      neighbors = [
-        [-1, 0],
-        [-1, 1],
-        [0, 1],
-        [1, 0],
-        [1, 1],
-      ];
-    } else if (i > 0 && i < shipLength) {
-      neighbors = [
-        [-1, 0],
-        [1, 0],
-      ];
-    }
-
+    let neighbors = this.getHorizontalNeighbors(i, shipLength);
     this.distributeBuffer(coord, neighbors);
   }
 
   markBufferZoneV(coord, i, shipLength) {
-    let neighbors;
-    if (shipLength == 1) {
-      neighbors = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, -1],
-        [0, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-      ];
-    } else if (i == 0) {
-      neighbors = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, -1],
-        [0, 1],
-      ];
-    } else if (i == shipLength - 1) {
-      neighbors = [
-        [0, -1],
-        [0, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-      ];
-    } else if (i > 0 && i < shipLength) {
-      neighbors = [
-        [0, -1],
-        [0, 1],
-      ];
-    }
+    let neighbors = this.getVerticalNeighbors(i, shipLength);
     this.distributeBuffer(coord, neighbors);
   }
 
@@ -240,7 +173,7 @@ class Gameboard {
       ) {
         // Mark the cell as part of the buffer zone, if it's not already part of a ship
         if (this.shipsArray[newX][newY] === null) {
-          this.shipsArray[newX][newY] = 1; // Use a distinctive marker for buffer zones
+          this.shipsArray[newX][newY] = 1;
         }
       }
     });
@@ -248,7 +181,6 @@ class Gameboard {
 
   // return true if all neighbors are null
   checkNeighbors(coord, neighbors) {
-    console.log(neighbors);
     for (let i = 0; i < neighbors.length; i++) {
       const newX = coord[0] + neighbors[i][0];
       const newY = coord[1] + neighbors[i][1];
@@ -287,8 +219,8 @@ class Gameboard {
 module.exports = Gameboard;
 
 let gameboard = new Gameboard();
-// gameboard.placeShip(3, [1, 1], "h");
-console.log(gameboard.isValidPosition(3, [7, 1], "h"));
+gameboard.placeShip(3, [1, 1], "h");
+console.log(gameboard.isValidPosition(1, [2, 2], "h"));
 
 // console.log(gameboard.placeShip(3, [1, 1], "h"));
 // console.log(gameboard.placeShip(3, [1, 1], "v"));
